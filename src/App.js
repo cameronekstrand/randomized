c08b084254863ff350170c26922a3e2707dc8bfb
seebey <cameron.ekstrand@gmail.com>
2021-09-28 11:05:39 -0500

    testing 10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "antd/dist/antd.css";
 import './App.css';
-import { BrowserRouter, BrowserRouter as Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Link, BrowserRouter as Switch, Route } from 'react-router-dom';
 import { Scripts } from './Components/Scripts';
 import { Home } from './Components/Home';
 import {Null} from './Components/Null';
 
 const App = () => (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Null />
           <div>
